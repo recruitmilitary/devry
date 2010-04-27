7e6f3089bbc009d55817af304d9c5af7623a0bd2
Michael Guterl <mguterl@gmail.com>
2010-04-27 13:48:03 -0400

    better extraction of description

--- lib/devry.rb
+++ lib/devry.rb
@@ -5,14 +5,17 @@ module Devry
   module Helpers
 
     def extract_text(element)
+      return nil if element.nil?
       element.text.strip
     end
 
     def extract_time(element)
-      Time.parse(element.text.strip)
+      return nil if element.nil?
+      Time.parse(extract_text(element))
     end
 
     def extract_url(element)
+      return nil if element.nil?
       element.at("a")['href']
     end
 
@@ -87,7 +90,7 @@ module Devry
 
     def description
       page = fetch_page(url)
-      ["tr:nth-child(11) p", "tr:nth-child(13) p", "tr:nth-child(15) p"].map do |selector|
+      ["tr:nth-child(11)", "tr:nth-child(13)", "tr:nth-child(15)"].map do |selector|
         extract_text page.at(selector)
       end.join("\n\n")
     end
