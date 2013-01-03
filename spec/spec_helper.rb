7ebd3d8c3e602b1f5ad5cc6981c04a71366d2843
Michael Guterl <michael@diminishing.org>
2013-01-03 15:29:40 -0500

    Update spec_helper for recent versions of rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'devry'
-require 'spec'
-require 'spec/autorun'
-
+require 'rspec'
 require 'fakeweb'
 
 FIXTURE_PATH = File.expand_path(File.dirname(__FILE__) + "/fixtures")
@@ -12,6 +10,3 @@ FakeWeb.allow_net_connect = false
 FakeWeb.register_uri(:get, "https://devryuniversity-devry.icims.com/jobs/search?pr=1", :response => FIXTURE_PATH + "/index.html")
 FakeWeb.register_uri(:get, "https://devryuniversity-devry.icims.com/jobs/search?pr=2", :response => FIXTURE_PATH + "/page_2.html")
 FakeWeb.register_uri(:get, "https://devryuniversity-devry.icims.com/jobs/13545/job",   :response => FIXTURE_PATH + "/job.html")
-
-Spec::Runner.configure do |config|
-end
